1e6a83cb0ed8f1607e88c6fb5ebef593c022b7de
Daniel Yordanov <theface11311@gmail.com>
2023-01-20 23:16:04 +0200

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,20 +1,37 @@
 import random
-
-computer_number = random.randint(1, 100)
-
+refuse_anther_game = False
 while True:
-    player_input = input('Guess the number (1 - 100): ')
-    if not player_input.isdigit():
-        print('Invalid input. Try again...')
-        continue
-    player_input = int(player_input)
-    if not(1 <= player_input <= 100):
-        print('Try again. Enter valid number between 1 and 100...')
-        continue
-    if player_input == computer_number:
-        print('You guess it!')
+    computer_number = random.randint(1, 100)
+
+    you_are_winner = False
+    tries = 8
+    while tries:
+        tries -= 1
+        player_input = input('Guess the number (1 - 100): ')
+        if not player_input.isdigit():
+            print('Invalid input. Try again...')
+            continue
+        player_input = int(player_input)
+        if not(1 <= player_input <= 100):
+            print('Try again. Enter valid number between 1 and 100...')
+            continue
+        if player_input == computer_number:
+            print('You guess it!')
+            you_are_winner = True
+            break
+        if player_input > computer_number:
+            print('Too high!')
+        else:
+            print('Too low!')
+    if not you_are_winner:
+        print('You need more practice!')
+    while True:
+        if input('Do you wanna another game?(Y/N)') == 'Y':
+            break
+        elif input('Do you wanna another game?(Y/N)') == 'N':
+            refuse_anther_game = True
+            break
+    if refuse_anther_game:
         break
-    if player_input > computer_number:
-        print('Too high!')
-    else:
-        print('Too low!')
+        
+
