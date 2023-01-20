e820152808052234eb8f429561af0911dd0ba11c
Daniel Yordanov <theface11311@gmail.com>
2023-01-21 00:09:20 +0200

    Update guess_the_number.py
    Added level system up to 21 level
    Added score system gets and take scores
    Added asking for another game
    Added prints of all these stuff.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,30 +1,56 @@
 import random
 refuse_anther_game = False
+
 while True:
-    computer_number = random.randint(1, 100)
+    score = 0
+    level = 1
+
 
     you_are_winner = False
     tries = 8
+    print('Your start level is 1!')
     while tries:
+        computer_number = random.randint(1, level * 100)
         tries -= 1
-        player_input = input('Guess the number (1 - 100): ')
+        player_input = input(f'Guess the number (1 - {level * 100}): ')
         if not player_input.isdigit():
             print('Invalid input. Try again...')
             continue
         player_input = int(player_input)
-        if not(1 <= player_input <= 100):
-            print('Try again. Enter valid number between 1 and 100...')
-            continue
         if player_input == computer_number:
             print('You guess it!')
-            you_are_winner = True
-            break
+            level += 1
+            print(f'You turn up to the next level => Your current level is {level}!')
+            score += 10
+            tries = 8
+            print(f'Your score is: {score}')
+            print(f'You have {tries} tries more!')
         if player_input > computer_number:
             print('Too high!')
+            print(f'You have {tries} tries more!')
+            score -= 2
+            if score < 0:
+                score = 0
+            print(f'Your score is: {score}')
         else:
             print('Too low!')
-    if not you_are_winner:
+            print(f'You have {tries} tries more!')
+            score -= 2
+            if score < 0:
+                score = 0
+            print(f'Your score is: {score}')
+    if not tries:
         print('You need more practice!')
+        while True:
+            if input('Do you wanna another game?(Y/N)') == 'Y':
+                break
+            elif input('Do you wanna another game?(Y/N)') == 'N':
+                refuse_anther_game = True
+                break
+        break
+    if level == 21:
+        print('The game is over. You are the WINNER!')
+        break
     while True:
         if input('Do you wanna another game?(Y/N)') == 'Y':
             break
@@ -33,5 +59,5 @@ while True:
             break
     if refuse_anther_game:
         break
-        
+
 
