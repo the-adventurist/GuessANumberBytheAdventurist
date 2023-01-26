e4662ed31203b1367c71d45808bd149faed337dc
Daniel Yordanov <theface11311@gmail.com>
2023-01-26 23:49:31 +0200

    Update guess_the_number.py
    Redundant printing - Fixed!
    Reduced difficulty - by reducing range of guesses. That now
    increase by 50 rather than 100.

--- guess_the_number.py
+++ guess_the_number.py
@@ -8,31 +8,34 @@ while True:
     you_are_winner = False
     tries = 8
     print('Your start level is 1!')
-    computer_number = random.randint(1, level * 100)
+    computer_number = random.randint(1, level * 50)
     while tries:
-        tries -= 1
-        player_input = input(f'Guess the number (1 - {level * 100}): ')
+        player_input = input(f'Guess the number (1 - {level * 50}): ')
         if not player_input.isdigit():
             print('Invalid input. Try again...')
             continue
+        if not(1 <= int(player_input) <= level * 50):
+            print(f'Try again! Choose number between 1 and {level * 50}')
+            continue
+        tries -= 1
         player_input = int(player_input)
         if player_input == computer_number:
             print('You guess it!')
             level += 1
             print(f'You turn up to the next level => Your current level is {level}!')
-            computer_number = random.randint(1, level * 100)
+            computer_number = random.randint(1, level * 50)
             score += 10
             tries = 8
             print(f'Your score is: {score}')
             print(f'You have {tries} tries more!')
-        if player_input > computer_number:
+        elif player_input > computer_number:
             print('Too high!')
             print(f'You have {tries} tries more!')
             score -= 2
             if score < 0:
                 score = 0
             print(f'Your score is: {score}')
-        else:
+        elif player_input < computer_number:
             print('Too low!')
             print(f'You have {tries} tries more!')
             score -= 2
@@ -40,12 +43,18 @@ while True:
                 score = 0
             print(f'Your score is: {score}')
 
-        if not tries or level == 21:
-            print('You need more practice!')
+    if not tries:
+        print('Game over!')
+        print(f'Your final score is: {score}')
+        print('You need more practice!')
+        while input() != 'Y' or 'N':
             if input('Do you wanna another game?(Y/N)').upper() == 'Y':
                 break
+
             elif input('Do you wanna another game?(Y/N)').upper() == 'N':
                 refuse_anther_game = True
                 break
-    break
-# proverka на
+        if refuse_anther_game:
+            break
+    if refuse_anther_game:
+        break
